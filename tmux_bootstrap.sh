f6f82ea0bfad3bf2a4803b1ea22af1a8219c53b2
Anton Paule <paule@fzi.de>
2017-09-01 13:25:15 +0200

    [update] | automatic tpm and plugins installation installation

--- tmux_bootstrap.sh
+++ tmux_bootstrap.sh
@@ -2,9 +2,6 @@
 
 export DOTFILES=$HOME/dotfiles
 
-# Install Tmux Plugin Manager
-git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-
 # Setup symlink to ~/.tmux.conf
 if [ -f $HOME/.tmux.conf ] || [ -h $HOME/.tmux.conf ]; then
   mv $HOME/.tmux.conf $HOME/.tmux.conf.backup
