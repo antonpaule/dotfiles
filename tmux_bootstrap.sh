251ce49a39d96f87175c9c81c5123e647437d441
Anton Paule <paule@fzi.de>
2017-09-01 12:59:15 +0200

    [fix] | home directory set correctly

--- tmux_bootstrap.sh
+++ tmux_bootstrap.sh
@@ -6,10 +6,9 @@ export DOTFILES=$HOME/dotfiles
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 
 # Setup symlink to ~/.tmux.conf
-if [ -f ~/.tmux.conf ] || [ -h ~/.tmux.conf ]; then
-  mv ~/.tmux.conf ~/.tmux.conf.backup
+if [ -f $HOME/.tmux.conf ] || [ -h $HOME/.tmux.conf ]; then
+  mv $HOME/.tmux.conf $HOME/.tmux.conf.backup
 fi
 
-$echo "Creating symlink to ~/.tmux.conf"
+$echo "Creating symlink to $HOME/.tmux.conf"
 ln -s $DOTFILES/tmux.conf $HOME/.tmux.conf
-
