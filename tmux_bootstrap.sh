dc90e151a1d0d2cb618864c06f29f2416e5c3cf1
Anton Paule <paule@fzi.de>
2017-09-01 13:19:32 +0200

    [fix] | type in log output for tmux_bootstrap script

--- tmux_bootstrap.sh
+++ tmux_bootstrap.sh
@@ -10,5 +10,5 @@ if [ -f $HOME/.tmux.conf ] || [ -h $HOME/.tmux.conf ]; then
   mv $HOME/.tmux.conf $HOME/.tmux.conf.backup
 fi
 
-$echo "Creating symlink to $HOME/.tmux.conf"
+echo "[Status][tmux] | Creating symlink to $HOME/.tmux.conf"
 ln -s $DOTFILES/tmux.conf $HOME/.tmux.conf
